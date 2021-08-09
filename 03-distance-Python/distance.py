a0dd342846c53ce02d2770b02455559074ed55ff
charan <charan2818@msitprogram.net>
2021-08-09 16:14:15 +0530

    solved distance

--- 03-distance-Python/distance.py
+++ 03-distance-Python/distance.py
@@ -3,7 +3,7 @@
 # that represent the two points (x1, y1) and (x2, y2), 
 # and returns the distance between those points as a int.
 
-
+import math
 def fun_distance(x1, y1, x2, y2):
-	# your code goes here
-	return 0
+	dist = math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+	return math.floor(dist)
