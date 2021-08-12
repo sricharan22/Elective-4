dfbf5db44e5b83059109c9bf667835b352b2a4ee
charan <charan2818@msitprogram.net>
2021-08-12 11:19:41 +0530

    done

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -6,4 +6,11 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
-	return False 
+	distSq = ((x1 - x2) **2) + ((y1 - y2) **2)
+	radSumSq = (r1 + r2) * (r1 + r2)
+	if (distSq == radSumSq):
+		return True
+	elif (distSq > radSumSq):
+		return False
+	else:
+		return True
